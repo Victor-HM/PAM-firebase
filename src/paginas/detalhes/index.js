3cd53d75669e898a5653bb207c4832b29c4f0655
unknown <you@example.com>
2022-09-16 14:41:47 -0300

    fim

--- src/paginas/detalhes/index.js
+++ src/paginas/detalhes/index.js
@@ -5,17 +5,12 @@ import { useRoute } from '@react-navigation/native';
 
 
 export default function Detalhes(){
-    const [nota,setNota] = useState();
     
-    const route = useRoute();
     return(
 
 
         <View>
-            <Text>{route.params.nome}</Text>
-            <Text>{route.params.nota1}</Text>
-            <Text>{route.params.nota2}</Text>
-            <Text>{route.params.nota3}</Text>
+           <Text> Detalhes  </Text>
             
         </View>
 
